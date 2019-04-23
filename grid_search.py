2c4848fbb5c714d0c6ec71384ff059ede2b629a1
mrfolfox4 <arranger1044@aim.com>
2019-04-22 20:46:37 -0700

    Updating to newest theano

--- grid_search.py
+++ grid_search.py
@@ -72,7 +72,7 @@ def stringify(x):
 
 CLUSTER_PYTHON_INTERPRETER = '/home/avergari/.local/bin/ipython'
 PYTHON_STR = 'ipython'
-MADE_TRAIN_STR = '{python} -- trainMADE.py {dataset} {params}'
+MADE_TRAIN_STR = 'THEANO_FLAGS=mode=FAST_RUN,device=cuda,floatX=float32 {python} -- trainMADE.py {dataset} {params}'
 
 
 def grid_search(python_int,
